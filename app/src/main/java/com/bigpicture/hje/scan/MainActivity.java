7ed4283cd58db1ada09beb802999b43c7a25f2a3
Juneui Hong <lowcrash33@gmail.com>
2017-04-11 21:16:16 +0900

    주석 for 둬

--- app/src/main/java/com/bigpicture/hje/scan/MainActivity.java
+++ app/src/main/java/com/bigpicture/hje/scan/MainActivity.java
@@ -17,16 +17,26 @@ public class MainActivity extends AppCompatActivity {
     //context 액티비티랑 같은 거라고 보면됨
     private Context context;
 
+    //Override는 extends AppCompatActivity 상속했을때 꼭 구현되어야 하는 함수로 자동 추가됨
+    //모든 activity 코드는 onCreate함수에서 시작 C의 main함수라고 보면됨
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //R은 Resource 의미 (각종 xml파일부터 이미지 파일등 R안에 관리됨), 메인화면 배포자?(activity_main.xml파일)를 여기 코드에서 관리하겠다
         setContentView(R.layout.activity_main);
         context = this;
+
+        //activity_main.xml 의 button_shop이라는 id의 버튼을 가져옴
         Button button_shop = (Button) findViewById(R.id.button_shop);
+
+        //버튼 눌렀을때 이벤트 처리
         button_shop.setOnClickListener(new Button.OnClickListener() {
             @Override
             public void onClick(View view) {
+                //intent는 현재 화면에서 특정 지시를 Broadcasting,  여기서는 화면 전환
                 Intent intent = new Intent(context, MapsActivity.class);
+                //지도 액티비티로 이동
                 startActivity(intent);
             } });
 
@@ -34,24 +44,33 @@ public class MainActivity extends AppCompatActivity {
         button_item.setOnClickListener(new Button.OnClickListener() {
             @Override
             public void onClick(View view) {
+                //바코드 화면에서 카메라 써야되는데 그전에 써도되는지 허락 받는거
+                //사용자가 이 앱에서 카메라 사용을 허락했는지 확인
                 if(ContextCompat.checkSelfPermission(context, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED){
+                    //쓴다고 했으면 zxing의 바코드 촬영 화면으로 전환
                     Intent intent = new Intent("com.google.zxing.client.android.SCAN");
+                    //위 지도맵 이동과 다르게 명령을 날릴때 아래와 같이 key-value 형식으로 값을 싸서 보낼수있음
                     intent.putExtra("SCAN_MODE", "ALL");
                     startActivityForResult(intent, 0);
                 }else{
+                    //사용자한테 카메라 쓸거냐고 물어보는거임
                     ActivityCompat.requestPermissions((Activity) context,new String[]{Manifest.permission.CAMERA},0);
                 }
             } });
     }
 
+    //바코드를 찍고 바코드 값 추출 후 다시 메인화면으로 돌아오면서 바코드 값을 돌려줌
+    //메인화면으로 다시 돌아왔을때 아래 함수 자동으로 호출 됨
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 
         if(requestCode == 0) {
-
+            //정상적으로 바코드 값을 가져왔으면
             if(resultCode == Activity.RESULT_OK)
             {
+                //되돌아온 Intent에서 key를 통해 value를 가져옴(바코드 숫자)
                 String contents = data.getStringExtra("SCAN_RESULT");
+                //토스트라고 화면하단에 잠깐 보여주고 없어지는거. 바코드 숫자 확인. 간단히 무슨 값 확인할때 좋음
                 Toast.makeText (context, contents, Toast.LENGTH_LONG).show();
 
             }
--- app/src/main/java/com/bigpicture/hje/scan/MapsActivity.java
+++ app/src/main/java/com/bigpicture/hje/scan/MapsActivity.java
@@ -14,9 +14,9 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
-    private GoogleMap mMap;
-    private MarkerOptions marker;
-    private LatLng testLatLng;
+    private GoogleMap mMap; //지도 자체에 대한 객체
+    private MarkerOptions marker; //마커라고 화면에 동그란거 뜨는거
+    private LatLng testLatLng; //위도, 경도 값을 가진 구조체 정도로 보면됨
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,12 +27,14 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
+        //마커 정의
         marker = new MarkerOptions();
+        //위도, 경도 (해커톤 장소)
         testLatLng = new LatLng(37.491, 127.020);
 
     }
 
-
+    //맵이 활성화 되면 자동 호출
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
@@ -40,8 +42,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         // Add a marker in Sydney and move the camera
         marker.position(testLatLng).title("Select shop position");
         mMap.addMarker(marker);
+        //해커톤 행사장으로 이동, 지도 확대를 15로
         mMap.moveCamera( CameraUpdateFactory.newLatLngZoom(testLatLng, 15) );
 
+        //맵 터치했을때 위,경도 토스트로 띄워주고 마커 위치 바꿔줌
         mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
 
             public void onMapClick(LatLng point) {
@@ -50,13 +54,14 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                         + point.longitude;
                 Toast.makeText(getApplicationContext(), text, Toast.LENGTH_LONG)
                         .show();
+                //기존 마커 삭제
                 mMap.clear();
                 marker.position(point).title("Click marker and add shop info.");
                 mMap.addMarker(marker);
             }
         });
 
-
+        //마커 눌렀을때 점포등록을 위한 추가 정보 입력 다이얼로그를 띄워야함
         mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
 
             public boolean onMarkerClick(Marker marker) {
