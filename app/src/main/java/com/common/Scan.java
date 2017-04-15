e91389b5984a45a65e784afa6db711c44d8e9143
Juneui Hong <lowcrash33@gmail.com>
2017-04-15 10:59:51 +0900

    AWS Server URL

--- app/src/main/java/com/common/Scan.java
+++ app/src/main/java/com/common/Scan.java
@@ -5,8 +5,8 @@ package com.common;
  */
 
 public class Scan {
-    public static String localUrl = "http://192.168.0.5:8080/isthere";
-    public static String serverUrl = "ec2-user@52.78.243.10/isthere";
+    //public static String localUrl = "http://192.168.0.5:8080/isthere";
+    public static String localUrl = "http://52.78.243.10:8080/isthere";
 
     public static String shopAdd = "/shop/add";
     public static String shopScan = "/shop/scan";
